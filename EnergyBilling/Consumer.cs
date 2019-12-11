07f4f40114bcca02a5f5e0f1e4afd8289a61cbd0
Choudhary, Yogesh <Yogesh.Choudhary@capita.co.uk>
2019-12-11 22:35:26 +0530

    initial commit

---
+++ EnergyBilling/Consumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Refactor exercise 1
+namespace EnergyBilling
+{
+    public class Consumer
+    {
+        public enum ConsumerType {  Individual = 0, Organisation = 1, GovernmentOrganisation = 2 };
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string BillingAddress { get; set; }
+        public decimal BilledAmount { get; set; }
+        public uint TotalUtilizedUnits { get; set; }
+        public uint AvgUnitsUtilizedPerDay { get; set; }
+        public int BilledMonth { get; set; }
+        public ConsumerType Type { get; set; }
+        public void CalculateBill()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            TotalUtilizedUnits = AvgUnitsUtilizedPerDay * (uint)DateTime.Now.Day;
+
+            if (TotalUtilizedUnits < 100)
+                BilledAmount = TotalUtilizedUnits;
+            else if (TotalUtilizedUnits < 200)
+                BilledAmount = TotalUtilizedUnits * 1.2m;
+            else if (TotalUtilizedUnits < 300)
+                BilledAmount = TotalUtilizedUnits * 1.4m;
+            else if (TotalUtilizedUnits < 500)
+                BilledAmount = TotalUtilizedUnits * 1.5m;
+
+            if (Type == ConsumerType.GovernmentOrganisation)
+                BilledAmount /= 2.0m;
+            if (Type == ConsumerType.Organisation)
+                BilledAmount *= 1.5m;
+        }
+
+        public void PrintBill()
+        {
+            BilledMonth = DateTime.Now.Month;
+            Console.WriteLine($"-----------------  Energy Bill ({ GetMonth(BilledMonth)}) -------------------------");
+            Console.WriteLine($"Consumer Id:{Id}         Type:{Type}");
+            Console.WriteLine($"Consumer Name:{Name}");
+            Console.WriteLine($"Address:{BillingAddress}");
+            Console.WriteLine($"....................................................................");
+            Console.Write($"Units consumed:{TotalUtilizedUnits}    Per day consumption:{AvgUnitsUtilizedPerDay}");
+            Console.WriteLine();
+            Console.WriteLine($"Billed amount: {BilledAmount} GBP");
+            Console.WriteLine($"--------------------------------------------------------------------");
+        }
+
+        private string GetMonth(int month)
+        {
+            string[] MonthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            return MonthNames[month - 1];
+        }
+    }
+}
---
+++ EnergyBillingConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyBillingConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnergyBilling.Consumer consumer = new EnergyBilling.Consumer
+            {
+                AvgUnitsUtilizedPerDay = (uint)(new Random(DateTime.Now.Second).Next(10, 20)),
+                Name = "John Doe",
+                BillingAddress = "12E, Stannard way, Chelmsford, Burlingtonshire, UA1 43B, United Kingdom",
+                Type = EnergyBilling.Consumer.ConsumerType.Individual
+            };
+
+            consumer.CalculateBill();
+            consumer.PrintBill();
+        }
+    }
+}
